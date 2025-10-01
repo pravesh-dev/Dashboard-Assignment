7aeb9a07b449fffa6daa38153e588b08564e1f47
pravesh dav <humantorch1932@gmail.com>
2025-10-01 14:36:37 +0530

    updated base url in utils

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://dashboard-assignment-backend-0894.onrender.com//api",
   withCredentials: true,
 });
 
